9d4d0132043b57c998f85393321581dbc25ddec7
EslamIbrahim007 <eslam.007w@gmail/com>
2026-02-17 19:05:18 +0200

    fixing some function in Auth

--- blog-backend/src/modules/auth/strategies/local.strategy.ts
+++ blog-backend/src/modules/auth/strategies/local.strategy.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
+// import { Injectable } from '@nestjs/common';
+// import { PassportStrategy } from '@nestjs/passport';
+// import { Strategy } from 'passport-local';
 
-import { AuthService } from '../auth.service';
-import { UnauthorizedException } from '@nestjs/common';
+// import { AuthService } from '../auth.service';
+// import { UnauthorizedException } from '@nestjs/common';
 
-@Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy,'local') {
-    constructor(private readonly authService: AuthService) {
-        super({ usernameField: 'email' });
-    }
+// @Injectable()
+// export class LocalStrategy extends PassportStrategy(Strategy,'local') {
+//     constructor(private readonly authService: AuthService) {
+//         super({ usernameField: 'email' });
+//     }
 
-    async validate(email: string, password: string): Promise<any> {
-        const user = await this.authService.verifyUser({ email, password });
-        if (!user) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-        return user;
-    }
-}
+//     async validate(email: string, password: string): Promise<any> {
+//         const user = await this.authService.verifyUser({ email, password });
+//         if (!user) {
+//             throw new UnauthorizedException('Invalid credentials');
+//         }
+//         return user;
+//     }
+// }
