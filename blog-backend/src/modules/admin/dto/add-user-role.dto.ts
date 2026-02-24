b5b6b67c3656a41d8ef0730b3541aca374264868
EslamOO7 <eslamebrahim598@gmail.com>
2026-02-25 01:06:53 +0200

    finishing the admin module and create the post module

--- blog-backend/src/modules/admin/dto/add-user-role.dto.ts
+++ blog-backend/src/modules/admin/dto/add-user-role.dto.ts
@@ -4,9 +4,5 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class AddUserRoleDto {
     @IsString()
     @IsNotEmpty()
-    userId: string;
-
-    @IsString()
-    @IsNotEmpty()
     roleName: 'Admin' | 'Author' | 'Reader';
 }
--- blog-backend/src/modules/auth/dto/forgot-password.dto.ts
+++ blog-backend/src/modules/auth/dto/forgot-password.dto.ts
@@ -1,3 +1,7 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class ForgotPasswordDto {
-  email: string;
-}   
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+}
