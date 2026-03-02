d79a44e40d18e7cdcf127f25879c92dd015a2ec7
EslamOO7 <eslamebrahim598@gmail.com>
2026-03-03 01:12:57 +0200

    post module

--- blog-backend/src/modules/posts/posts.module.ts
+++ blog-backend/src/modules/posts/posts.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { PostTranslation } from './entities/post-translation.entity';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post, PostTranslation])],
-  controllers: [],
-  providers: [],
+  controllers: [PostsController],
+  providers: [PostsService],
+  exports: [PostsService],
 })
 export class PostsModule { }
