6c4ed0a6ed5af9db80f54a7c2df99a68122aa932
EslamIbrahim007 <eslam.007w@gmail/com>
2026-03-03 04:36:25 +0200

    edit package

--- blog-backend/src/migrations/1772505114911-AddPostsFinal.ts
+++ blog-backend/src/migrations/1772505114911-AddPostsFinal.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddPosts1772481315875 implements MigrationInterface {
-    name = 'AddPosts1772481315875'
+export class AddPostsFinal1772505114911 implements MigrationInterface {
+    name = 'AddPostsFinal1772505114911'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "post_translations" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "post_id" uuid NOT NULL, "language" character varying(5) NOT NULL, "title" character varying(255) NOT NULL, "slug" character varying(255) NOT NULL, "excerpt" character varying(255) NOT NULL, "content" text NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "UQ_e54e7e89ee895deed98002a1993" UNIQUE ("language", "slug"), CONSTRAINT "UQ_1875c0986bedf4f97258d521d99" UNIQUE ("post_id", "language"), CONSTRAINT "PK_977f23a9a89bf4a1a9e2e29c136" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_11f143c8b50a9ff60340edca47" ON "post_translations" ("post_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_34390d3aa850c97492f1acef33" ON "post_translations" ("slug") `);
         await queryRunner.query(`CREATE TYPE "public"."posts_status_enum" AS ENUM('draft', 'published')`);
-        await queryRunner.query(`CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "status" "public"."posts_status_enum" NOT NULL DEFAULT 'draft', "published_at" TIMESTAMP WITH TIME ZONE, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "authorId" character varying NOT NULL, "author_id" uuid, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "status" "public"."posts_status_enum" NOT NULL DEFAULT 'draft', "published_at" TIMESTAMP WITH TIME ZONE, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "authorId" character varying NOT NULL, "author_id" uuid, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_c5a322ad12a7bf95460c958e80" ON "posts" ("authorId") `);
         await queryRunner.query(`ALTER TABLE "post_translations" ADD CONSTRAINT "FK_11f143c8b50a9ff60340edca475" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "posts" ADD CONSTRAINT "FK_312c63be865c81b922e39c2475e" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
